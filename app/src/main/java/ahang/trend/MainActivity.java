08952e047deefb6a1387472c64804933a5e997e3
ahangchen <1418659400@qq.com>
2016-08-24 00:27:16 +0800

    tab layout, move use less icon, color reset

--- app/src/main/java/ahang/trend/MainActivity.java
+++ app/src/main/java/ahang/trend/MainActivity.java
@@ -1,9 +1,10 @@
 package ahang.trend;
 
+import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,78 +14,112 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import ahang.trend.adapter.ViewPagerAdapter;
+
 public class MainActivity extends AppCompatActivity
-		implements NavigationView.OnNavigationItemSelectedListener {
+		implements NavigationView.OnNavigationItemSelectedListener,
+		GithubTrendFragment.OnFragmentInteractionListener,
+		ArxivFragment.OnFragmentInteractionListener,
+		NotesFragment.OnFragmentInteractionListener {
+	private Toolbar toolbar;
+	private DrawerLayout drawer;
+	private NavigationView navigationView;
+	private ViewPager viewPager;
+	private TabLayout tabLayout;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+	private void initViewPager() {
+		viewPager = (ViewPager) findViewById(R.id.viewpager);
+		ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+		adapter.addFragment(GithubTrendFragment.newInstance(), getString(R.string.github));
+		adapter.addFragment(ArxivFragment.newInstance(), getString(R.string.arxiv));
+		adapter.addFragment(NotesFragment.newInstance(), getString(R.string.notes));
+		viewPager.setAdapter(adapter);
+	}
+	private void initTabs() {
+		tabLayout = (TabLayout) findViewById(R.id.tabs);
+		assert tabLayout != null;
+		tabLayout.setupWithViewPager(viewPager);
+	}
+	private void init() {
+		toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
-
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+		drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
 				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 		drawer.setDrawerListener(toggle);
 		toggle.syncState();
 
-		NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+		navigationView = (NavigationView) findViewById(R.id.nav_view);
+		assert navigationView != null;
 		navigationView.setNavigationItemSelectedListener(this);
+		initViewPager();
+		initTabs();
+	}
+
+	private boolean closeDrawer() {
+		if (drawer.isDrawerOpen(GravityCompat.START)) {
+			drawer.closeDrawer(GravityCompat.START);
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		init();
 	}
 
 	@Override
 	public void onBackPressed() {
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-		if (drawer.isDrawerOpen(GravityCompat.START)) {
-			drawer.closeDrawer(GravityCompat.START);
-		} else {
+		 if(!closeDrawer()) {
 			super.onBackPressed();
 		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		for (int i = 0; i < menu.size(); i++) {
+			menu.getItem(i).getIcon().setColorFilter(getResources().getColor(R.color.colorControlNormal), PorterDuff.Mode.SRC_ATOP);
+		}
 		return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
+		switch (item.getItemId()) {
+			case R.id.action_settings:
+				return true;
 		}
-
 		return super.onOptionsItemSelected(item);
 	}
 
-	@SuppressWarnings("StatementWithEmptyBody")
+
 	@Override
 	public boolean onNavigationItemSelected(MenuItem item) {
 		// Handle navigation view item clicks here.
-		int id = item.getItemId();
-
-		if (id == R.id.nav_camera) {
-			// Handle the camera action
-		} else if (id == R.id.nav_gallery) {
-
-		} else if (id == R.id.nav_slideshow) {
-
-		} else if (id == R.id.nav_share) {
-
-		} else if (id == R.id.nav_send) {
-
+		switch (item.getItemId()) {
+			case R.id.nav_camera:
+				break;
+			case R.id.nav_gallery:
+				break;
+			case R.id.nav_slideshow:
+				break;
+			case R.id.nav_share:
+				break;
+			case R.id.nav_send:
+				break;
 		}
-
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 		drawer.closeDrawer(GravityCompat.START);
 		return true;
+	}
+
+	@Override
+	public void onFragmentInteraction(Uri uri) {
+
 	}
 }
